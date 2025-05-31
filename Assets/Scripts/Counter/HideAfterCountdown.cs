8013807575e561e1f2a45a79fc45eb4cd8f21edc
Kimera Royal <kai.anderson.ellis.brown@gmail.com>
2025-05-31 23:18:07 +0100

    Added objects disappearing after X days

--- Assets/Scripts/Counter/HideAfterCountdown.cs
+++ Assets/Scripts/Counter/HideAfterCountdown.cs
@@ -6,15 +6,24 @@ namespace Kitty
     {
         private DayCounter m_counter;
 
+        [SerializeField] private int m_afterDays;
+
         private void Awake()
         {
             m_counter = FindAnyObjectByType<DayCounter>();
             m_counter.OnStateUpdated.AddListener(OnStateUpdated);
+            m_counter.OnDisplayDateUpdated.AddListener(OnDisplayDateUpdated);
         }
 
         private void OnStateUpdated(CounterState _state)
         {
-            if(_state != CounterState.PostCountdown) { return; }
+            if(m_afterDays > 0 || _state != CounterState.PostCountdown) { return; }
+            gameObject.SetActive(false);
+        }
+
+        private void OnDisplayDateUpdated(int _displayDate)
+        {
+            if(m_afterDays < 1 || m_counter.CurrentState != CounterState.PostCountdown || _displayDate <= m_afterDays) { return; }
             gameObject.SetActive(false);
         }
     }
